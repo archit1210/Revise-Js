4f18f3e3b9418acc562904b2b15c87388add3b96
archit1210 <architlokhande72@gmail.com>
2024-10-30 01:35:09 +0530

    First code and datatypes completed

---
+++ Datatypes.js
@@ -0,0 +1,24 @@
+{
+    var x = 25
+    console.log(x)
+}
+
+console.log(x)
+
+/// var declaration method does'nt follows scopes and re-assignment is allowed
+
+let age = 93
+console.log(age)
+age = 56
+console.log(age)
+age = "Sixty Nine"
+console.log(age)
+// Js is dynamically types language
+
+const num = 10
+console.log(num)
+// const variables are declared and assigned only once in a program and can't be updated or modified
+
+let area;
+console.log(area);
+

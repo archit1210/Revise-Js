c06b0634e10b2231b1b2c9b3cae1365e94960fdb
archit1210 <architlokhande72@gmail.com>
2024-10-31 01:00:18 +0530

    Functions completed

---
+++ Functions.js
@@ -0,0 +1,24 @@
+// function sayName(name) {
+//     console.log(`${name}`)
+// }
+
+// sayName("Archit Lokhande")
+
+function getAverage(n1, n2) {
+    return (n1 + n2)
+}
+
+console.log(getAverage(6, 2))
+
+const ans = function (n1, n2) {
+    return n1 * n2
+}
+
+console.log(ans(2, 27))
+
+//Arrow function
+const power = (n1, n2) => {
+    return n1 ** n2
+}
+
+console.log(power(2, 5))

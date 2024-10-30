5223e2baa309d1e13f4ef7fed1a5d01d9f1f5eb8
archit1210 <architlokhande72@gmail.com>
2024-10-31 00:30:12 +0530

    strings completed

---
+++ strings.js
@@ -0,0 +1,16 @@
+let firstName = "Archit "
+let lastName = " Tukaram Lokhande"
+let name = `My name is John`
+let s = new String("Jerry")
+
+console.log(name)
+console.log(s)
+
+let fullName = firstName + lastName;
+console.log(fullName)
+console.log(fullName.length)
+console.log(fullName.toUpperCase())
+console.log(fullName.toLowerCase())
+console.log(fullName.substring(2))
+console.log(fullName.substring(2, 6))
+console.log(fullName.split(' '))

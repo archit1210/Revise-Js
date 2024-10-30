c7f22f9ecaca06557f40f78aab17ee2fcab47686
archit1210 <architlokhande72@gmail.com>
2024-10-30 12:02:49 +0530

    operators completed

---
+++ Operators.js
@@ -0,0 +1,61 @@
+//arithmatic operators
+//binary operator
+let a = 2
+let b = 5
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(b % a)
+console.log(a ** b)
+
+//unary operator
+console.log(++a);
+console.log(a++);
+console.log(a);
+
+//assignment operator
+a += 10;
+b -= 2
+
+console.log(a);
+console.log(b);
+
+//comparison operator
+/*console.log(10 > 20);
+console.log(10 < 30);
+console.log(10 >= 10);
+console.log(20 <= 10);*/
+console.log('5' === 5);
+
+//ternary operator
+let age = 20
+let ans = (age > 18) ? "Voting possible" : "Not Possible"
+console.log(ans);
+
+//Logical operator
+let x = (true && true && false)
+console.log(x);
+
+
+// bitwise operator 
+console.log(2 & 5);
+console.log(2 | 5);
+console.log(2 ^ 5);
+console.log(2 ^ 2);
+
+//left shift and right shift
+console.log(10 << 2); /* output = 10 * 2^2 */
+console.log(10 >> 1); /* output = 10 / 2^2 */
+
+
+
+
+
+
+
+
+
+
+
+
